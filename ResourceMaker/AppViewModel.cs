3f433a919820f83d2c6a3ce5923f0254fe0adb5d
Maskl <masklod@gmail.com>
2013-01-05 19:48:34 +0100

    Example project

---
+++ ResourceMaker/AppViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace ResourceMaker
+{
+  [Export(typeof(AppViewModel))]
+  public class AppViewModel : PropertyChangedBase, IHandle<ColorEvent>
+  {
+    private SolidColorBrush _Color;
+
+    [ImportingConstructor]
+    public AppViewModel(ColorViewModel colorModel, IEventAggregator events)
+    {
+      ColorModel = colorModel;
+
+      // Get the event aggregator through the constructor and subscribe this ColorViewModel so it can listen for ColorEvent messages.
+      events.Subscribe(this);
+    }
+
+    public ColorViewModel ColorModel { get; private set; }
+
+    // This property is for changing the color of the rectangle.
+    public SolidColorBrush Color
+    {
+      get { return _Color; }
+      set
+      {
+        _Color = value;
+        NotifyOfPropertyChange(() => Color);
+      }
+    }
+
+    // This method is called after a ColorEvent message is published from somewhere else in the application.
+    public void Handle(ColorEvent message)
+    {
+      Color = message.Color;
+    }
+  }
+}
