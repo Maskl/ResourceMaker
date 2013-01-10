08cb73f2b060b20c21259ce2b592639b0a22d1d0
Maskl <masklod@gmail.com>
2013-01-11 00:02:06 +0100

    Resource edit window

---
+++ ResourceMaker/Int32ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ResourceMaker
+{
+    static class Int32ExtensionMethods
+    {
+        public static bool Contains(this Int32Rect instance, Point point)
+        {
+            return new Rect(instance.X, instance.Y, instance.Width, instance.Height).Contains(point);
+        }
+    }
+}
